9121c529c6fffbd5177a9193900cd4a06b821f67
mahian.maksud <mahian.maksud@fdmgroup.com>
2023-11-16 10:05:19 +0800

    Initial commit

---
+++ src/main/java/com/maikro/checkoutSystem/CheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.maikro.checkoutSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maikro/checkoutSystem/CheckoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maikro.checkoutSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
