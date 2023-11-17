4deda59e417289bc4bc6e2a07a3c40144ee5b4aa
mahian.maksud <mahian.maksud@fdmgroup.com>
2023-11-17 15:42:18 +0800

    added annotation for mapping child class to parent abstract class

--- src/main/java/com/maikro/checkoutSystem/model/DiscountByProduct.java
+++ src/main/java/com/maikro/checkoutSystem/model/DiscountByProduct.java
@@ -1,8 +1,10 @@
 package com.maikro.checkoutSystem.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 
 @Entity
+@PrimaryKeyJoinColumn(name = "discountId")
 public class DiscountByProduct extends Discount{
 
 }
