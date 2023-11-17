b589af2737d1061b078ebd2056ffb79c66114833
mahian.maksud <mahian.maksud@fdmgroup.com>
2023-11-17 22:30:05 +0800

    implemented the addNewProduct method to add new product object into the database

--- src/main/java/com/maikro/checkoutSystem/service/ProductService.java
+++ src/main/java/com/maikro/checkoutSystem/service/ProductService.java
@@ -1,8 +1,19 @@
 package com.maikro.checkoutSystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.maikro.checkoutSystem.model.Product;
+import com.maikro.checkoutSystem.repository.ProductRepo;
 
 @Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepo productRepo;
+	
+	public void addNewProduct(Product product) {
+		productRepo.save(product);
+	}
 
 }
