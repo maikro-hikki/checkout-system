e618794089595f1e34dacf26ba8f6ec1813c20f6
mahian maksud <mahian.maksud@fdmgroup.com>
2023-11-18 12:48:31 +0800

    added tests for DiscountService class methods

---
+++ src/test/java/com/maikro/checkoutSystem/service/DiscountServiceTest.java
@@ -0,0 +1,113 @@
+package com.maikro.checkoutSystem.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.maikro.checkoutSystem.model.Admin;
+import com.maikro.checkoutSystem.model.Discount;
+import com.maikro.checkoutSystem.model.DiscountByProduct;
+import com.maikro.checkoutSystem.model.DiscountByQuantity;
+import com.maikro.checkoutSystem.repository.DiscountRepo;
+
+@SpringBootTest
+class DiscountServiceTest {
+	
+	@Autowired
+    private DiscountRepo discountRepo;
+
+    @Autowired
+    private DiscountService discountService;
+    
+    @Autowired
+    private UserClassService userClassService;
+
+	@Test
+	void testAddDiscountByQuantity() {
+		
+		//create an admin to make a discount
+    	Admin admin = new Admin();
+    	userClassService.addAdminUser(admin);
+    	//create a quantity discount and set it under the admin
+        DiscountByQuantity discount = new DiscountByQuantity();
+        discount.setAdmin(admin);
+        
+        //add the created discount into the repository
+        discountService.addDiscountByQuantity(discount);
+        
+        //use the discountRepo.findById method to find discount in repo
+        Discount savedDiscount = discountRepo.findById(discount.getDiscountId()).orElse(null);
+        
+        //check that the discount exists in the repo or not
+        assertThat(savedDiscount).isNotNull();
+        assertThat(savedDiscount.getDiscountId()).isEqualTo(discount.getDiscountId());
+        
+	}
+	
+	@Test
+	void testAddDiscountByProduct() {
+		
+		//create an admin to make a discount
+    	Admin admin = new Admin();
+    	userClassService.addAdminUser(admin);
+    	//create a product discount and set it under the admin
+        DiscountByProduct discount = new DiscountByProduct();
+        discount.setAdmin(admin);
+        
+        //add the created discount into the repository
+        discountService.addDiscountByProduct(discount);
+        
+        //use the discountRepo.findById method to find discount in repo
+        Discount savedDiscount = discountRepo.findById(discount.getDiscountId()).orElse(null);
+        
+        //check that the discount exists in the repo or not
+        assertThat(savedDiscount).isNotNull();
+        assertThat(savedDiscount.getDiscountId()).isEqualTo(discount.getDiscountId());
+        
+	}
+	
+	@Test
+    public void testRemoveDiscountById_for_DiscountByQuantity() {
+    	
+		//create an admin to make a discount
+    	Admin admin = new Admin();
+    	userClassService.addAdminUser(admin);
+    	//create a quantity discount and set it under the admin
+        DiscountByQuantity discount = new DiscountByQuantity();
+        discount.setAdmin(admin);
+        
+        //add the created discount into the repository
+        discountService.addDiscountByQuantity(discount);
+        
+        //call the method to remove the discount
+        discountService.removeDiscountById(discount.getDiscountId());
+        
+        //verify that the product is no longer in the repository
+        assertThat(discountRepo.findById(discount.getDiscountId())).isEmpty();
+        
+    }
+	
+	@Test
+    public void testRemoveDiscountById_for_DiscountByProduct() {
+    	
+		//create an admin to make a discount
+    	Admin admin = new Admin();
+    	userClassService.addAdminUser(admin);
+    	//create a product discount and set it under the admin
+        DiscountByProduct discount = new DiscountByProduct();
+        discount.setAdmin(admin);
+        
+        //add the created discount into the repository
+        discountService.addDiscountByProduct(discount);
+        
+        //call the method to remove the discount
+        discountService.removeDiscountById(discount.getDiscountId());
+        
+        //verify that the product is no longer in the repository
+        assertThat(discountRepo.findById(discount.getDiscountId())).isEmpty();
+        
+    }
+
+}
