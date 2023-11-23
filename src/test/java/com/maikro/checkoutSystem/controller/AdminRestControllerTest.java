4744d99c97b634fd4eb49b46b1cc2d7c14fed3c3
mahian maksud <mahian.maksud@fdmgroup.com>
2023-11-23 18:38:03 +0800

    added rest assured dependencies

--- src/test/java/com/maikro/checkoutSystem/controller/AdminRestControllerTest.java
+++ src/test/java/com/maikro/checkoutSystem/controller/AdminRestControllerTest.java
@@ -1,14 +1,10 @@
-package com.maikro.checkoutSystem.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class AdminRestControllerTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//package com.maikro.checkoutSystem.controller;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.hamcrest.Matchers.*;
+//
+//@SpringBootTest
+//public class AdminControllerTest {
+//
+//    
+//}
