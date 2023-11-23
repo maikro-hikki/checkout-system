199dc1257dc38d59ae9f41c25fb7f1e67ca2ee98
mahian maksud <mahian.maksud@fdmgroup.com>
2023-11-23 15:27:04 +0800

    modifies tests to reflect recent source code changes

--- src/test/java/com/maikro/checkoutSystem/service/DiscountServiceTest.java
+++ src/test/java/com/maikro/checkoutSystem/service/DiscountServiceTest.java
@@ -2,117 +2,93 @@ package com.maikro.checkoutSystem.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.maikro.checkoutSystem.constants.ProductType;
 import com.maikro.checkoutSystem.model.Admin;
+import com.maikro.checkoutSystem.model.Customer;
 import com.maikro.checkoutSystem.model.Discount;
 import com.maikro.checkoutSystem.model.DiscountByProduct;
 import com.maikro.checkoutSystem.model.DiscountByQuantity;
+import com.maikro.checkoutSystem.model.Product;
 import com.maikro.checkoutSystem.repository.DiscountRepo;
 
 @SpringBootTest
 class DiscountServiceTest {
-	
-	@Autowired
-    private DiscountRepo discountRepo;
 
-    @Autowired
-    private DiscountService discountService;
-    
-    @Autowired
-    private UserClassService userClassService;
+	@Autowired
+	private DiscountRepo discountRepo;
+
+	@Autowired
+	private DiscountService discountService;
+
+	@Autowired
+	private UserClassService userClassService;
+
+	Admin admin = new Admin("admin", "123", "John", "Doe");
+	DiscountByQuantity discount1 = new DiscountByQuantity(admin, 3, 0.2);
+	DiscountByProduct discount2 = new DiscountByProduct(admin, 0.1);
+
+	@BeforeEach
+	void setUp() {
+
+		userClassService.addAdminUser(admin);
+		discountService.addDiscountByQuantity(discount1);
+		discountService.addDiscountByProduct(discount2);
+	}
 
 	@Test
 	@DirtiesContext
 	void testAddDiscountByQuantity_ShouldAddTypeDiscountByQuantity_ToDataBase() {
-		
-		//create an admin to make a discount
-    	Admin admin = new Admin();
-    	userClassService.addAdminUser(admin);
-    	//create a quantity discount and set it under the admin
-        DiscountByQuantity discount = new DiscountByQuantity();
-        discount.setAdmin(admin);
-        
-        //add the created discount into the repository
-        discountService.addDiscountByQuantity(discount);
-        
-        //use the discountRepo.findById method to find discount in repo
-        Discount savedDiscount = discountRepo.findById(discount.getDiscountId()).orElse(null);
-        
-        //check that the discount exists in the repo or not
-        assertThat(savedDiscount).isNotNull();
-        assertThat(savedDiscount.getDiscountId()).isEqualTo(discount.getDiscountId());
-        
+
+		// use the discountRepo.findById method to find discount in repo
+		Discount savedDiscount = discountRepo.findById(discount1.getDiscountId()).orElse(null);
+
+		// check that the discount exists in the repo or not
+		assertThat(savedDiscount).isNotNull();
+		assertThat(savedDiscount.getDiscountId()).isEqualTo(discount1.getDiscountId());
+
 	}
-	
+
 	@Test
 	@DirtiesContext
 	void testAddDiscountByProduct_ShouldAddTypeDiscountByProduct_ToDataBase() {
-		
-		//create an admin to make a discount
-    	Admin admin = new Admin();
-    	userClassService.addAdminUser(admin);
-    	//create a product discount and set it under the admin
-        DiscountByProduct discount = new DiscountByProduct();
-        discount.setAdmin(admin);
-        
-        //add the created discount into the repository
-        discountService.addDiscountByProduct(discount);
-        
-        //use the discountRepo.findById method to find discount in repo
-        Discount savedDiscount = discountRepo.findById(discount.getDiscountId()).orElse(null);
-        
-        //check that the discount exists in the repo or not
-        assertThat(savedDiscount).isNotNull();
-        assertThat(savedDiscount.getDiscountId()).isEqualTo(discount.getDiscountId());
-        
+
+		// use the discountRepo.findById method to find discount in repo
+		Discount savedDiscount = discountRepo.findById(discount2.getDiscountId()).orElse(null);
+
+		// check that the discount exists in the repo or not
+		assertThat(savedDiscount).isNotNull();
+		assertThat(savedDiscount.getDiscountId()).isEqualTo(discount2.getDiscountId());
+
 	}
-	
+
 	@Test
 	@DirtiesContext
-    void testRemoveDiscountById_ShouldRemoveTypeDiscountByQuantity_FromDatabase() {
-    	
-		//create an admin to make a discount
-    	Admin admin = new Admin();
-    	userClassService.addAdminUser(admin);
-    	//create a quantity discount and set it under the admin
-        DiscountByQuantity discount = new DiscountByQuantity();
-        discount.setAdmin(admin);
-        
-        //add the created discount into the repository
-        discountService.addDiscountByQuantity(discount);
-        
-        //call the method to remove the discount
-        discountService.removeDiscountById(discount.getDiscountId());
-        
-        //verify that the product is no longer in the repository
-        assertThat(discountRepo.findById(discount.getDiscountId())).isEmpty();
-        
-    }
-	
+	void testRemoveDiscountById_ShouldRemoveTypeDiscountByQuantity_FromDatabase() {
+
+		// call the method to remove the discount
+		discountService.removeDiscountById(discount1.getDiscountId());
+
+		// verify that the product is no longer in the repository
+		assertThat(discountRepo.findById(discount1.getDiscountId())).isEmpty();
+
+	}
+
 	@Test
 	@DirtiesContext
-    void testRemoveDiscountById_ShouldRemoveTypeDiscountByProduct_FromDatabase() {
-    	
-		//create an admin to make a discount
-    	Admin admin = new Admin();
-    	userClassService.addAdminUser(admin);
-    	//create a product discount and set it under the admin
-        DiscountByProduct discount = new DiscountByProduct();
-        discount.setAdmin(admin);
-        
-        //add the created discount into the repository
-        discountService.addDiscountByProduct(discount);
-        
-        //call the method to remove the discount
-        discountService.removeDiscountById(discount.getDiscountId());
-        
-        //verify that the product is no longer in the repository
-        assertThat(discountRepo.findById(discount.getDiscountId())).isEmpty();
-        
-    }
+	void testRemoveDiscountById_ShouldRemoveTypeDiscountByProduct_FromDatabase() {
+
+		// call the method to remove the discount
+		discountService.removeDiscountById(discount2.getDiscountId());
+
+		// verify that the product is no longer in the repository
+		assertThat(discountRepo.findById(discount2.getDiscountId())).isEmpty();
+
+	}
 
 }
