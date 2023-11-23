175debcd24233fd0c5f22a1204ec254fd6fb5af4
mahian maksud <mahian.maksud@fdmgroup.com>
2023-11-23 15:40:57 +0800

    modifies tests to reflect recent source code changes

--- src/main/java/com/maikro/checkoutSystem/service/DiscountByQuantityService.java
+++ src/main/java/com/maikro/checkoutSystem/service/DiscountByQuantityService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.maikro.checkoutSystem.Utility;
 import com.maikro.checkoutSystem.model.DiscountByQuantity;
 import com.maikro.checkoutSystem.repository.DiscountByQuantityRepo;
 
--- src/test/java/com/maikro/checkoutSystem/service/ProductDiscountServiceTest.java
+++ src/test/java/com/maikro/checkoutSystem/service/ProductDiscountServiceTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.maikro.checkoutSystem.constants.ProductType;
 import com.maikro.checkoutSystem.model.Admin;
 import com.maikro.checkoutSystem.model.Discount;
 import com.maikro.checkoutSystem.model.DiscountByProduct;
@@ -38,26 +40,39 @@ class ProductDiscountServiceTest {
 	@Autowired
 	private UserClassService userClassService;
 
+	Admin admin = new Admin("admin", "123", "John", "Doe");
+	DiscountByQuantity discount1 = new DiscountByQuantity(admin, 3, 0.2);
+	DiscountByProduct discount2 = new DiscountByProduct(admin, 0.1);
+	DiscountByQuantity discount3 = new DiscountByQuantity(admin, 3, 0.2);
+	DiscountByProduct discount4 = new DiscountByProduct(admin, 0.1);
+	Product product = new Product("Apple TV", 10.5, 50, ProductType.ELECTRONICS);
+	Product product1 = new Product("Apple TV", 5.5, 30, ProductType.ELECTRONICS);
+	Product product2 = new Product("Samsung TV", 20.5, 70, ProductType.ELECTRONICS);
+
+	@BeforeEach
+	void setUp() {
+
+		userClassService.addAdminUser(admin);
+		discountService.addDiscountByQuantity(discount1);
+		discountService.addDiscountByProduct(discount2);
+		discountService.addDiscountByQuantity(discount3);
+		discountService.addDiscountByProduct(discount4);
+		product.setAdmin(admin);
+		product1.setAdmin(admin);
+		product2.setAdmin(admin);
+		productService.addNewProduct(product);
+		productService.addNewProduct(product1);
+		productService.addNewProduct(product2);
+
+	}
+
 	@Test
 	@DirtiesContext
 	void testAddProductDiscountByProductAndDiscount_ShouldAddProductDiscountInDatabaseByProductIdAndDiscountId_Return1ForTheTest() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
-
 		// add discount to product
-		productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(), discount.getDiscountId());
+		productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(),
+				discount1.getDiscountId());
 
 		// Use productDiscountRepo to look for the added product discount
 		List<ProductDiscount> productDiscount = productDiscountRepo.findByProductProductId(product.getProductId());
@@ -70,28 +85,14 @@ class ProductDiscountServiceTest {
 	@DirtiesContext
 	void testAddProductDiscountByProductAndDiscount_ShouldNotAddProductDiscountInDatabaseIfDiscountAlreadyAppliedToProduct_OnlyHave1DiscountForProduct() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
-
 		// add discount to product, should return true as discount was not applied to
 		// product yet
 		assertTrue(productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(),
-				discount.getDiscountId()));
+				discount1.getDiscountId()));
 		// discount already applied to product and should not be added to database,
 		// should return false
 		assertFalse(productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(),
-				discount.getDiscountId()));
+				discount1.getDiscountId()));
 
 		// Use productDiscountRepo to look for the added product discount
 		List<ProductDiscount> productDiscount = productDiscountRepo.findByProductProductId(product.getProductId());
@@ -105,48 +106,17 @@ class ProductDiscountServiceTest {
 	@DirtiesContext
 	void testDiscountAlreadyAppliedToProduct_ForDiscountAlreadyAppliedToProduct_ShouldReturnTrue() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
-
 		// add discount to product
-		productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(), discount.getDiscountId());
+		productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(),
+				discount1.getDiscountId());
 
 		assertTrue(productDiscountService.discountAlreadyAppliedToProduct(product.getProductId(),
-				discount.getDiscountId()));
+				discount1.getDiscountId()));
 	}
 
 	@Test
 	@DirtiesContext
 	void testDiscountAlreadyAppliedToProduct_ShouldReturnFalse_IfDiscountNotYetAppliedToProduct() {
-
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount1 = new DiscountByQuantity();
-		discount1.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount1);
-		// create a product discount and set it under the admin
-		DiscountByProduct discount2 = new DiscountByProduct();
-		discount2.setAdmin(admin);
-		discountService.addDiscountByProduct(discount2);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
 
 		// add discount to product
 		productDiscountService.addProductDiscountByProductAndDiscount(product.getProductId(),
@@ -160,22 +130,8 @@ class ProductDiscountServiceTest {
 	@DirtiesContext
 	void testAddProductDiscount_ShouldAddProductDiscountInDatabaseByProductDiscountObject_ReturnTrueForValidInput() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
-
 		// create new product discount object
-		ProductDiscount productDiscount = new ProductDiscount(product, discount);
+		ProductDiscount productDiscount = new ProductDiscount(product, discount1);
 
 		// add product discount object into the database, should return true
 		assertTrue(productDiscountService.addProductDiscount(productDiscount));
@@ -192,20 +148,12 @@ class ProductDiscountServiceTest {
 	@DirtiesContext
 	void testAddProductDiscount_ShouldNotAddProductDiscountInDatabaseIfProductOrDiscountNotInDatabase_ReturnFalseForInvalidInput() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
+		// not added to database/does not exist
+		Product product7 = new Product("Some TV", 10.5, 10, ProductType.ELECTRONICS);
+		DiscountByQuantity discount7 = new DiscountByQuantity(admin, 3, 0.2);
 
 		// create new product discount object
-		ProductDiscount productDiscount = new ProductDiscount(product, discount);
+		ProductDiscount productDiscount = new ProductDiscount(product7, discount7);
 
 		// product and discount were not added to database so invalid input, should
 		// return false
@@ -222,20 +170,6 @@ class ProductDiscountServiceTest {
 	@Test
 	@DirtiesContext
 	void testAddProductDiscount_ShouldNotAddProductDiscountInDatabaseIfDiscountAlreadyAppliedToProduct_ReturnFalse() {
-
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount1 = new DiscountByQuantity();
-		discount1.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount1);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
 
 		// create new product discount object
 		ProductDiscount productDiscount1 = new ProductDiscount(product, discount1);
@@ -259,24 +193,6 @@ class ProductDiscountServiceTest {
 	@Test
 	@DirtiesContext
 	void testFindByProductIdAndDiscountId_ShouldReturnProductDiscountObjectWithProductIdAndDiscountId_CreatedProductDiscountShouldMatchRetrieved() {
-
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount1 = new DiscountByQuantity();
-		discount1.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount1);
-		// create product discount and set it under the admin
-		DiscountByProduct discount2 = new DiscountByProduct();
-		discount2.setAdmin(admin);
-		discountService.addDiscountByProduct(discount2);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
 
 		// create new product discount object
 		ProductDiscount productDiscount1 = new ProductDiscount(product, discount1);
@@ -310,30 +226,16 @@ class ProductDiscountServiceTest {
 
 	@Test
 	@DirtiesContext
-	void testRemoveProductDiscountByProductIdAndDiscountId_ShouldRemoveProductDiscount_ReturnTrueIfProductDiscountExist() {
-
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount = new DiscountByQuantity();
-		discount.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
+	void testRemoveProductDiscountByProductIdAndDiscountId_ShouldRemoveProductDiscount_ReturnTrueIfSuccessfullyRemoved() {
 
 		// create new product discount object
-		ProductDiscount productDiscount = new ProductDiscount(product, discount);
+		ProductDiscount productDiscount = new ProductDiscount(product, discount1);
 
 		// add the productDiscount to database
 		productDiscountService.addProductDiscount(productDiscount);
 
 		assertTrue(productDiscountService.removeProductDiscountByProductIdAndDiscountId(product.getProductId(),
-				discount.getDiscountId()));
+				discount1.getDiscountId()));
 		// Use productDiscountRepo to look for the failed to add product discount
 		ProductDiscount retrievedProductDiscount = productDiscountRepo.findById(productDiscount.getProductDiscountId())
 				.orElse(null);
@@ -345,28 +247,6 @@ class ProductDiscountServiceTest {
 	@Test
 	@DirtiesContext
 	void testRemoveProductDiscountByProductIdAndDiscountId_ShouldReturnFalse_IfProductDiscountIsNotInDatabase() {
-
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount1 = new DiscountByQuantity();
-		discount1.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount1);
-		// create a product discount and set it under the admin
-		DiscountByProduct discount2 = new DiscountByProduct();
-		discount2.setAdmin(admin);
-		discountService.addDiscountByProduct(discount2);
-
-		// create a product and set it under the admin
-		Product product1 = new Product();
-		product1.setAdmin(admin);
-		productService.addNewProduct(product1);
-		// create a product and set it under the admin
-		Product product2 = new Product();
-		product2.setAdmin(admin);
-		productService.addNewProduct(product2);
 
 		// create new product discount object
 		ProductDiscount productDiscount = new ProductDiscount(product1, discount1);
@@ -391,36 +271,18 @@ class ProductDiscountServiceTest {
 	@DirtiesContext
 	void testFindDiscountsByProductId_ShouldReturnDiscountsOnProduct() {
 
-		// create an admin to make a discount
-		Admin admin = new Admin();
-		userClassService.addAdminUser(admin);
-
-		// create a quantity discount and set it under the admin
-		DiscountByQuantity discount1 = new DiscountByQuantity();
-		discount1.setAdmin(admin);
-		discountService.addDiscountByQuantity(discount1);
-		// create a product discount and set it under the admin
-		DiscountByProduct discount2 = new DiscountByProduct();
-		discount2.setAdmin(admin);
-		discountService.addDiscountByProduct(discount2);
-
-		// create a product and set it under the admin
-		Product product = new Product();
-		product.setAdmin(admin);
-		productService.addNewProduct(product);
-
 		// create new product discount objects
 		ProductDiscount productDiscount1 = new ProductDiscount(product, discount1);
 		ProductDiscount productDiscount2 = new ProductDiscount(product, discount2);
 
 		// add the productDiscount to database
-		productDiscountService.addProductDiscount(productDiscount1);		
+		productDiscountService.addProductDiscount(productDiscount1);
 		productDiscountService.addProductDiscount(productDiscount2);
-		
-		//retrieve all discounts on a product
+
+		// retrieve all discounts on a product
 		List<Discount> discounts = productDiscountService.findDiscountsByProductId(product.getProductId());
-		
-		//discountIds should match in the returned list
+
+		// discountIds should match in the returned list
 		assertEquals(discount1.getDiscountId(), discounts.get(0).getDiscountId());
 		assertEquals(discount2.getDiscountId(), discounts.get(1).getDiscountId());
 
